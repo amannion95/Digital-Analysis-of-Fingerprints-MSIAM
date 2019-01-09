f87c428abb79fb9386c127916096ca74321b55e1
triloa <46493219+triloa@users.noreply.github.com>
2019-01-09 17:00:09 +0100

    Add files via upload

--- infra/Project_Infra/src/Picture.cpp
+++ infra/Project_Infra/src/Picture.cpp
@@ -1,26 +1,35 @@
 #include "Picture.h"
 #include "Usefull_functions.h"
-#include <cassert>
+
 using namespace cv;
-using namespace std;
+
 
 
 
 Picture::Picture(const std::string& filename){
-  picture=cv::imread(filename);
-  x_length=(picture.size()).height;
-  y_length=(picture.size()).width;
+  picture=imread(filename);
+  x_length=(picture.size()).width;
+  y_length=(picture.size()).height;
 }
 
-
 Picture::Picture(unsigned int x_length,unsigned int y_length){
-  Mat image(y_length,x_length,CV_8UC1 );
+  Mat image(y_length,x_length,CV_8UC1);
+  picture=image.clone();
+  this->x_length=x_length;
+  this->y_length=y_length;
 }
 
 Picture::Picture(const cv::Mat& pic){
   picture=pic.clone();
   x_length=(picture.size()).width;
   y_length=(picture.size()).height;
+}
+
+Picture::Picture(){
+  x_length=0;
+  y_length=0;
+  Mat image(0,0,CV_8UC1);
+  picture=image;
 }
 
 float Picture::get_intensity(unsigned int i, unsigned int j)const{
@@ -35,9 +44,54 @@ void Picture::set_intensity(unsigned int i, unsigned int j,float intensity){
 }
 
 void Picture::print_picture()const{
+  std::cout<<"X_len : "<<x_length<<" Y_len : "<<y_length<<std::endl;
   namedWindow("Display Image", WINDOW_NORMAL );
   imshow("Display Image", picture);
   waitKey(0);
+}
+
+
+Picture Picture::symmetry_wrt_y()const{
+  Picture symmetry;
+  symmetry=clone();
+  std::cout<<symmetry.x_length<<std::endl;
+  for(int j=0;j<y_length;j++){
+    for(int i=0;i<x_length;i++){
+      symmetry.set_intensity(j,x_length-1-i,get_intensity(j,i));
+    }
+  }
+
+  return symmetry;
+}
+
+
+Picture Picture::symmetry_wrt_x()const{
+  Picture symmetry;
+  symmetry=clone();
+  for(int j=0;j<y_length;j++){
+    for(int i=0;i<x_length;i++){
+      symmetry.set_intensity(y_length-1-j,i,get_intensity(j,i));
+    }
+  }
+
+  return symmetry;
+}
+Picture Picture::diagonal_symmetry_top_to_bottom()const{
+  Picture sym(picture.t());
+  return sym;
+}
+
+
+
+Picture Picture::diagonal_symmetry_bottom_to_top()const{
+  Picture sym(picture.t());
+  return sym.symmetry_wrt_y().symmetry_wrt_x();
+}
+
+void Picture::operator=(Picture Pic){
+  picture=(Pic.picture).clone();
+  x_length=Pic.x_length;
+  y_length=Pic.y_length;
 }
 
 float Picture::maximum_intensity()const{
@@ -48,18 +102,33 @@ float Picture::maximum_intensity()const{
       }
     }
    return(max_intensity);
- }
+}
 
 float Picture::minimum_intensity()const{
   float min_intensity=1.;
-  for (int i = 0 ; i < x_length ; i++ ){
-    for (int j = 0 ; j < y_length ; j++ ){
-      min_intensity=min(get_intensity(i,j),min_intensity);
+    for (int i = 0 ; i < x_length ; i++ ){
+      for (int j = 0 ; j < y_length ; j++ ){
+        min_intensity=min(get_intensity(i,j),min_intensity);
+      }
     }
-  }
   return(min_intensity);
 }
 
+Picture Picture::clone()const{
+  Picture clone;
+  clone.picture=picture.clone();
+  clone.x_length=x_length;
+  clone.y_length=y_length;
+  return clone;
+}
+
+
+unsigned int Picture::get_x_len(){
+  return x_length;
+}
+unsigned int Picture::get_y_len(){
+  return y_length;
+}
 
 void Picture::rescale_color(){
   float min_intensity = minimum_intensity();
--- infra/Project_Infra/src/main.cpp
+++ infra/Project_Infra/src/main.cpp
@@ -14,6 +14,9 @@ using namespace std;
 
 int main(int argc, char** argv )
 {
+
+
+
   Mat image(200,200,CV_8UC1,125);
   Mat image2 = imread( "../../data/blurred_finger.png" , IMREAD_COLOR );
 
@@ -37,6 +40,30 @@ int main(int argc, char** argv )
   img_square.rescale_color();
   img_square.print_picture();
     waitKey(0);
-    return 0;
+
+    /*Mat image2 = imread( "../../data/squares.png" , IMREAD_GRAYSCALE );
+    //cvtColor( image, image2, CV_BGR2GRAY );
+    namedWindow("Display Image", WINDOW_NORMAL );
+
+
+    Picture pic(image2);
+
+    Mat img (300,200,CV_8UC1);
+    Picture img_pic(img);
+
+    for(int j=0;j<img_pic.get_y_len()-50 ;j++){
+      for(int i=0;i<img_pic.get_x_len()-50;i++){
+        img_pic.set_intensity(j,i,1);
+    }
+  }
+  //pic.print_picture();
+  //pic.symmetry_wrt_y().print_picture();
+  //pic.symmetry_wrt_x().print_picture();
+  //img_pic.print_picture();
+  //img_pic.symmetry_wrt_y().print_picture();
+  //img_pic.diagonal_symmetry_top_to_bottom().print_picture();
+  //img_pic.diagonal_symmetry_bottom_to_top().print_picture();
+  */
+  return 0;
 
 }
