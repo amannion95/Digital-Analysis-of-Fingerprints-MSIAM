1d3a422e79e0db1e5514987674d16261e90a532c
triloa <46493219+triloa@users.noreply.github.com>
2019-01-09 15:18:49 +0100

    Add files via upload

--- infra/Project_Infra/src/Picture.cpp
+++ infra/Project_Infra/src/Picture.cpp
@@ -12,7 +12,7 @@ Picture::Picture(const std::string& filename){
   y_length=(picture.size()).width;
 }
 
-Picture::Picture(unsigned int x_length=0,unsigned int y_length=0){
+Picture::Picture(unsigned int x_length,unsigned int y_length){
   Mat image(y_length,x_length,CV_8UC1);
   picture=image.clone();
   this->x_length=x_length;
@@ -21,8 +21,15 @@ Picture::Picture(unsigned int x_length=0,unsigned int y_length=0){
 
 Picture::Picture(const cv::Mat& pic){
   picture=pic.clone();
-  x_length=(picture.size()).width;
-  y_length=(picture.size()).height;
+  x_length=(picture.size()).height;
+  y_length=(picture.size()).width;
+}
+
+Picture::Picture(){
+  x_length=0;
+  y_length=0;
+  Mat image(0,0,CV_8UC1);
+  picture=image;
 }
 
 float Picture::get_intensity(unsigned int i, unsigned int j)const{
@@ -37,6 +44,7 @@ void Picture::set_intensity(unsigned int i, unsigned int j,float intensity){
 }
 
 void Picture::print_picture()const{
+  std::cout<<"X_len : "<<x_length<<" Y_len : "<<y_length<<std::endl;
   namedWindow("Display Image", WINDOW_NORMAL );
   imshow("Display Image", picture);
   waitKey(0);
@@ -44,17 +52,71 @@ void Picture::print_picture()const{
 
 
 Picture Picture::symmetry_transform()const{
-  Picture symmetry(x_length,y_length);
-  for(int i=0;i<x_length;i++){
-    for(int j=0;j<y_length;j++){
-      symmetry.set_intensity(y_length-1-j,x_length-1-i,this->get_intensity(j,i));
+  Picture symmetry;
+  symmetry=clone();
+  std::cout<<symmetry.x_length<<std::endl;
+  for(int j=0;j<y_length;j++){
+    for(int i=0;i<x_length;i++){
+      symmetry.set_intensity(j,x_length-1-i,get_intensity(j,i));
+    }
+  }
+
+  return symmetry;
+}
+
+Picture Picture::diagonal_symmetry_up_to_()const{
+  Picture symmetry;
+  symmetry=clone();
+  std::cout<<symmetry.x_length<<std::endl;
+  for(int j=0;j<y_length;j++){
+    for(int i=0;i<x_length;i++){
+      symmetry.set_intensity(i,j,get_intensity(j,i));
     }
   }
   return symmetry;
 }
+Picture diagonal_symmetry_low_to_()const{}
+
+
 
 void Picture::operator=(Picture Pic){
   picture=(Pic.picture).clone();
   x_length=Pic.x_length;
   y_length=Pic.y_length;
+}
+
+float Picture::maximum_intensity()const{
+  float max_intensity=0.;
+    for (int i = 0 ; i < x_length ; i++ ){
+      for (int j = 0 ; j < y_length ; j++ ){
+        max_intensity=max(get_intensity(i,j),max_intensity);
+      }
+    }
+   return(max_intensity);
+ }
+
+float Picture::minimum_intensity()const{
+  float min_intensity=1.;
+    for (int i = 0 ; i < x_length ; i++ ){
+      for (int j = 0 ; j < y_length ; j++ ){
+        min_intensity=min(get_intensity(i,j),min_intensity);
+      }
+    }
+  return(min_intensity);
+}
+
+Picture Picture::clone()const{
+  Picture clone;
+  clone.picture=picture.clone();
+  clone.x_length=x_length;
+  clone.y_length=y_length;
+  return clone;
+}
+
+
+unsigned int Picture::get_x_len(){
+  return x_length;
+}
+unsigned int Picture::get_y_len(){
+  return y_length;
 }
--- infra/Project_Infra/src/Picture.h
+++ infra/Project_Infra/src/Picture.h
@@ -5,6 +5,7 @@
 #include <opencv2/opencv.hpp>
 #include <stdlib.h>
 #include "Usefull_functions.h"
+#include <algorithm>    // std::max
 
 
 class Picture{
@@ -18,14 +19,20 @@ class Picture{
     Picture(const std::string& filename);
     Picture(unsigned int x_length,unsigned int y_length);
     Picture(const cv::Mat& pic);
-
+    Picture();
     float get_intensity(unsigned int i, unsigned int j)const;
-
     void set_intensity(unsigned int i, unsigned int j,float intensity);
     void operator=(Picture pic);
+    unsigned int get_x_len();
+    unsigned int get_y_len();
     void print_picture()const;
-
+    float maximum_intensity()const;
+    float minimum_intensity()const;
     Picture symmetry_transform()const;
+    Picture diagonal_symmetry_up_to_()const;
+    Picture diagonal_symmetry_low_to_()const;
+    Picture clone()const;
+
 };
 
 
