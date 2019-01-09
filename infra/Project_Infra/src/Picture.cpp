861b54fe79d2952bb5a810a048522a2cdd524d50
Maxime Fays <faysm@F216-07.e-im2ag.ujf-grenoble.fr>
2019-01-09 12:10:33 +0100

    Adding some constructors to Picture.cpp and some basic methods

--- infra/Project_Infra/src/Picture.cpp
+++ infra/Project_Infra/src/Picture.cpp
@@ -2,16 +2,25 @@
 #include "Usefull_functions.h"
 
 using namespace cv;
-
+using namespace std;
 
 
 
 Picture::Picture(const std::string& filename){
   picture=cv::imread(filename);
+  x_length=(picture.size()).height;
+  y_length=(picture.size()).width;
+}
+
+
+Picture::Picture(unsigned int x_length,unsigned int y_length){
+  Mat image(y_length,x_length,CV_8UC1 );
 }
 
 Picture::Picture(const cv::Mat& pic){
   picture=pic.clone();
+  x_length=(picture.size()).width;
+  y_length=(picture.size()).height;
 }
 
 float Picture::get_intensity(unsigned int i, unsigned int j)const{
@@ -23,4 +32,30 @@ void Picture::set_intensity(unsigned int i, unsigned int j,float intensity){
     std::cerr<<"Wrong intensity value, she must belong to [0,1]"<<std::endl;
   }
   picture.at<uchar>(i,j)=iftoi(intensity);
+}
+
+void Picture::print_picture()const{
+  namedWindow("Display Image", WINDOW_NORMAL );
+  imshow("Display Image", picture);
+  waitKey(0);
+}
+
+float Picture::maximum_intensity(){
+  float max_intensity=0.;
+    for (int i = 0 ; i < x_length ; i++ ){
+      for (int j = 0 ; j < y_length ; j++ ){
+        max_intensity=max(get_intensity(i,j),max_intensity);
+      }
+    }
+   return(max_intensity);
+ }
+
+float Picture::minimum_intensity(){
+  float min_intensity=1.;
+    for (int i = 0 ; i < x_length ; i++ ){
+      for (int j = 0 ; j < y_length ; j++ ){
+        min_intensity=min(get_intensity(i,j),min_intensity);
+      }
+    }
+  return(min_intensity);
 }
--- infra/Project_Infra/src/main.cpp
+++ infra/Project_Infra/src/main.cpp
@@ -17,20 +17,13 @@ int main(int argc, char** argv )
 
   Mat image2 = imread( "../../data/blurred_finger.png" , IMREAD_COLOR );
 
+  namedWindow("Display Image", WINDOW_NORMAL );
+
+  Picture img(image2);
+
+  cout << img.maximum_intensity() << " "  << img.minimum_intensity() << "\n";
 
 
-    Mat image(100,100,CV_8UC1,21);
-
-    namedWindow("Display Image", WINDOW_NORMAL );
-
-
-
-    for(int j=0;j<=100  ;j++){
-      for(int i=100;i<=199;i++){
-        image2.at<uchar>(j,i)=0;
-
-}
-}
   imshow("Display Image", image2);
     waitKey(0);
     return 0;
