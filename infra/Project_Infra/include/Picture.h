c7770bf73045c4d9a1369479fd9500a68941e92e
littlefire95 <aidan.mannion@outlook.com>
2019-01-10 11:20:46 +0100

    update include folder

--- infra/Project_Infra/include/Picture.h
+++ infra/Project_Infra/include/Picture.h
@@ -4,21 +4,38 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <stdlib.h>
+#include "Usefull_functions.h"
+#include <algorithm>    // std::max
+
 
 class Picture{
+
   private:
+
     cv::Mat picture;
     unsigned int x_length, y_length;
 
   public:
     Picture(const std::string& filename);
+    Picture(unsigned int x_length,unsigned int y_length);
     Picture(const cv::Mat& pic);
-    
+    Picture();
     float get_intensity(unsigned int i, unsigned int j)const;
-
     void set_intensity(unsigned int i, unsigned int j,float intensity);
-
+    void operator=(Picture pic);
+    unsigned int get_x_len();
+    unsigned int get_y_len();
+    void print_picture()const;
+    float maximum_intensity()const;
+    float minimum_intensity()const;
+    Picture symmetry_wrt_y()const;
+    Picture symmetry_wrt_x()const;
+    Picture diagonal_symmetry_top_to_bottom()const;
+    Picture diagonal_symmetry_bottom_to_top()const;
+    Picture clone()const;
+    void rescale_color();
 };
+
 
 
 
--- infra/Project_Infra/include/Usefull_functions.h
+++ infra/Project_Infra/include/Usefull_functions.h
@@ -5,6 +5,24 @@
 
 
 
+/*  ------
+iitof : Takes color intensity as an int between [0,255] and converts it into a float between [0,1]
+----     */
+float iitof(int color);
+
+/*  ------
+iftoi : Takes color intensity a float between [0,255] and converts it into an int between [0,1]
+----     */
+int iftoi(float color);
+
+/*  ------
+display_mat : Takes a matrix, it's number of row, it's number of col and display it
+----     */
+void display_matrix(float ** matrix,int row, int col);
+
+
+
+
 
 
 
